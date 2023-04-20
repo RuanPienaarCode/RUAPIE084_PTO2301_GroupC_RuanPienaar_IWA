84ef1fa811686af7ffc90b810f4fa7feeeaaa6fc
Ruan Pienaar <ruan3d@gmail.com>
2023-04-20 07:13:32 +0200

    Add IWA2 Challanges

--- IWA1/script.js
+++ IWA1/script.js
@@ -1,13 +1,22 @@
 const MAX_NUMBER = 15;
 const MIN_NUMBER = -5;
 const STEP_AMOUNT = 5;
-
+/*
+ *Use alt Q - to compress comments.
+ *Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+ *Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+ *when an unknown printer took a galley of type and scrambled it to make a type
+ *specimen book. It has survived not only five centuries, but also the leap into
+ *electronic typesetting, remaining essentially unchanged. It was popularised in
+ *the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+ *and more recently with desktop publishing software like Aldus PageMaker
+ *including versions of Lorem Ipsum.
+ */
 const number = document.querySelector('[data-key="number"]');
 const add = document.querySelector('[data-key="add"]');
 const subtract = document.querySelector('[data-key="subtract"]');
 
 const subtractHandler = () => {
-  //   console.log("Subtract was clicked");
   const newValue = parseInt(number.value) - STEP_AMOUNT;
   number.value = newValue;
 
@@ -19,7 +28,6 @@ const subtractHandler = () => {
   }
 };
 const addHandler = () => {
-  //   console.log("Add was clicked");
   const newValue = parseInt(number.value) + STEP_AMOUNT;
   number.value = newValue;
   if (subtract.disabled === true) {
